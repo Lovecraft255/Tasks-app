38d62c04f0e012ba4397dfd4af77495f1851eadb
Lovecraft255 <dilanbemitez6@gmail.com>
2021-11-06 18:27:23 -0300

    Funcione de eliminar tareas agregada, estilo de letras terminado

--- dom.js
+++ dom.js
@@ -18,23 +18,36 @@ function createTask(){
 
 function addTask( task ){
 
+    const eliminateBtn = document.createElement("button");
+
+    eliminateBtn.classList.add("fa-trash-alt");
+
+    eliminateBtn.classList.add("fas");
+
     const taskDiv = document.createElement("div");
     taskDiv.classList.add("bordes");
 
     const titleH4 = document.createElement("h4");
     const descP = document.createElement("p");
 
-    titleH4.textContent = taskName;
-    descP.textContent = taskDesc;
+    titleH4.textContent = task.name;
+    descP.textContent = task.desc;
 
     taskDiv.setAttribute( "id" , taskCount);
 
+    titleH4.style.fontFamily='UItalica'
+    descP.style.fontFamily='Ubi'
+
     taskDiv.append( titleH4 );
     taskDiv.append( descP );
+    taskDiv.append( eliminateBtn );
 
     penndingTaskDiv.append( taskDiv );
 
 }
+
+
+
 
 let btnAdd = document.getElementById("btn-add");
 
@@ -46,9 +59,29 @@ let penndingTaskDiv = document.getElementById("pendig-tasks");
 
 let taskCount = 0;
 
+penndingTaskDiv.addEventListener( "click" , (e) => {
+
+    console.log( e.target.tagName );
+
+    if( e.target.tagName == "BUTTON" ){
+
+        console.log( e.target.parentElement );
+
+        const parent = e.target.parentElement;
+
+        parent.remove();
+
+        alert( "Se eliminara la tarea" );
+
+    }
+
+} )
+
 btnAdd.addEventListener( "click" , () => {
 
     const task = createTask();
+
+    console.log(task);
 
     penndingTask.push( task );
 
