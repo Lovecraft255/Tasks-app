cc750550f4332cd95899b9c3acbe01e831671386
Lovecraft255 <dilanbemitez6@gmail.com>
2021-11-20 18:07:45 -0300

    Funcion de completar tareas agregadas

--- dom.js
+++ dom.js
@@ -17,31 +17,51 @@ function createTask(){
 }
 
 function addTask( task ){
-
+    
     const eliminateBtn = document.createElement("button");
+
+    const checkTask = document.createElement("input");
+
+    const checkDesc = document.createElement("label");
+
+    checkTask.setAttribute("type" , "checkbox");
+
+    checkDesc.textContent = "Marcar tarea como completada";
 
     eliminateBtn.classList.add("fa-trash-alt");
 
     eliminateBtn.classList.add("fas");
 
+    eliminateBtn.classList.add("button-size");
+
     const taskDiv = document.createElement("div");
+
     taskDiv.classList.add("bordes");
 
     const titleH4 = document.createElement("h4");
     const descP = document.createElement("p");
+    const descID = document.createElement("p");
 
     titleH4.textContent = task.name;
     descP.textContent = task.desc;
+    descID.textContent = task.id;
 
     taskDiv.setAttribute( "id" , taskCount);
 
+
+
     titleH4.style.fontFamily='UItalica'
     descP.style.fontFamily='Ubi'
+    descID.style.fontFamily='Ubi'
+    checkDesc.style.fontFamily='Ubi'
 
     taskDiv.append( titleH4 );
     taskDiv.append( descP );
+    taskDiv.append( descID );
     taskDiv.append( eliminateBtn );
-
+    taskDiv.append( checkTask );
+    taskDiv.append( checkDesc);
+  
     penndingTaskDiv.append( taskDiv );
 
 }
@@ -56,6 +76,8 @@ const penndingTask = [];
 const completeTask = [];
 
 let penndingTaskDiv = document.getElementById("pendig-tasks");
+
+let completeTaskDiv = document.getElementById("complete-tasks");
 
 let taskCount = 0;
 
@@ -73,9 +95,28 @@ penndingTaskDiv.addEventListener( "click" , (e) => {
 
         alert( "Se eliminara la tarea" );
 
+    } else if( e.target.getAttribute("type") == "checkbox" ){
+
+        const parent = e.target.parentElement;
+
+        let parentId= parent.getAttribute('id');
+
+        let tarea = penndingTask.splice( parentId , 1);
+
+        console.log(tarea);
+
+        completeTask.push( tarea );
+
+        console.log( completeTask );
+
+        parent.remove();
+
+        completeTaskDiv.append( parent );
+
     }
 
 } )
+  
 
 btnAdd.addEventListener( "click" , () => {
 
